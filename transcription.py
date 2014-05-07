b542ac56cec299e239b6456b07512f2097b4d5f6
scoavoux <samuel.coavoux@gmail.com>
2014-05-07 16:37:18 +0200

    attempts at integrating libvlc -- failed

--- transcription.py
+++ transcription.py
@@ -1,7 +1,8 @@
 import sublime, sublime_plugin
-# from . import vlc
 import sys
 import os
+import time
+from . import vlc
 from . import yaml
 
 #feed command to vlc socket to get the time played in seconds
@@ -11,6 +12,50 @@ vlcout = os.path.join(workingdir,'vlc.out')
 
 # audiofile = "/home/scoavoux/Dropbox/Thèse/Data/Observation-entretiens MBA/Enregistrements entretiens en salle/40 - 19072012 Femme, conservatrice, école du Louvre, Strasbourg - Benedicte Herbage.WAV"
 # audiofile = "file://" + audiofile
+
+def yamlparser(view):
+	# look for yaml blocks
+	yaml_position = view.find_all("---")
+	
+	# Make sure there is an even number of "---" (YAML blocks delimiter)
+	if len(yaml_position)%2 == 1:
+		print("Error : YAML blocks not defined properly")
+
+	YAML = ""
+	# Isolate YAML blocks
+	for i in range(0,len(yaml_position),2):
+		start = yaml_position[i].b
+		stop = yaml_position[i+1].a
+		YAML += view.substr(sublime.Region(start,stop))
+		
+	# Parse YAML Blocks
+	parsed_yaml = yaml.load(YAML)
+
+	return(parsed_yaml)
+
+# class TranscriptionGetFilePathCommand(sublime_plugin.TextCommand):
+# 	def run(self,view):
+# 		parsed_yaml = yamlparser(self.view)
+# 		filename = parsed_yaml["file"]
+# 		# filepath = "file"
+# 		# Adjust for relative path
+# 		return(filename)
+
+# class VlcLaunchCommand(sublime_plugin.ApplicationCommand):
+#     def run(self):
+#     	filename = "/home/scoavoux/Dropbox/Thèse/Data/Observation-entretiens MBA/Enregistrements entretiens en salle/Thèse41 - 20072012 - Femme, employée banque de france, retraite (1).WAV"
+#     	sublime.instance = vlc.Instance()
+#     	sublime.mediaplayer = sublime.instance.media_player_new()
+#     	sublime.media = sublime.instance.media_new(filename)
+#     	sublime.mediaplayer.set_media(sublime.media)
+		
+# class VlcPlayCommand(sublime_plugin.ApplicationCommand):
+# 	def run(self):
+# 		sublime.mediaplayer.play()
+# 		print("playing")
+# 		time.sleep(5)
+# 		sublime.mediaplayer.pause()
+# 		print("stop playing")
 
 class VlcStartCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -32,22 +77,7 @@ class TranscriptionNewLineCommand(sublime_plugin.TextCommand):
 		point = view.sel()[0].b
 		line = view.substr(view.line(view.sel()[0]))
 		
-		# look for yaml blocks
-		yaml_position = view.find_all("---")
-		
-		# Make sure there is an even number of "---" (YAML blocks delimiter)
-		if len(yaml_position)%2 == 1:
-			print("Error : YAML blocks not defined properly")
-
-		YAML = ""
-		# Isolate YAML blocks
-		for i in range(0,len(yaml_position),2):
-			start = yaml_position[i].b
-			stop = yaml_position[i+1].a
-			YAML += view.substr(sublime.Region(start,stop))
-		
-		# Parse YAML Blocks
-		parsed_yaml = yaml.load(YAML)
+		parsed_yaml = yamlparser(view)
 
 		# Extract Interviewer and Respondant names
 		interviewer = parsed_yaml["interviewer"]
@@ -93,6 +123,8 @@ class TranscriptionNewLineCommand(sublime_plugin.TextCommand):
 # 	+ retour rapide
 # 	+ monte/baisse son
 # 	+ prends le temps
+
+# Mode dictée à la 
 
 # Liste d'interlocuteurs en YAML
 # Insérer nom d'interlocuteur
